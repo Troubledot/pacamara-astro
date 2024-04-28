3e2e176306aa7ae205a0ff29c9a8591833d681d3
zhaokaiwen <763018250@qq.com>
2024-04-28 17:43:58 +0800

    refactor(blog): update packages
    update modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  site: "https://pacamara-astro-6y7xr.kinsta.page"
+    integrations: [tailwind(), mdx(), icon()],
+    site: 'https://pacamara-astro-6y7xr.kinsta.page'
 });
